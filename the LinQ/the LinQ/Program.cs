131da35cb8a266988c51ed19567f996848cad9c3
Lubadnl <112851821+Lubadnl@users.noreply.github.com>
2022-09-05 10:45:13 +0300

    Add files via upload

---
+++ the LinQ/the LinQ/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace example
+{
+    class Program123
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = { "simcha", "sasson", "gil", "rina", "aliza", "tzila", "ron" };
+            //var q= arr.Where(name => name.Length > 3);
+            //IEnumerable<string> q = arr.Where(name => name.Length > 3).Skip(2).Take(2).Reverse();
+
+
+            //*** query operators
+            var q = from name in arr
+                    where name.Length > 3
+                    select name;
+
+            foreach (string i in q)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
