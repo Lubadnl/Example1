17ec9726dc12d9c11dc403f6c795f2ed68d01806
Lubadnl <112851821+Lubadnl@users.noreply.github.com>
2022-09-12 10:39:20 +0300

    Add files via upload

---
+++ StoreProcedureCompany/StoreProcedureCompany/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+
+SqlConnection cn = new SqlConnection("Data Source=.;Initial Catalog=Northwind;Integrated Security=True");
+SqlCommand com = new SqlCommand("CompanyNameForCustId", cn);
+com.CommandType = CommandType.StoredProcedure;
+
+SqlParameter p1 = new SqlParameter("@custid", SqlDbType.NChar);
+p1.Value = "ANATR";
+com.Parameters.Add(p1);
+
+com.Parameters.Add(new SqlParameter("@compname", SqlDbType.NVarChar, 40));
+com.Parameters[1].Direction = ParameterDirection.Output;
+cn.Open();
+
+com.ExecuteNonQuery();
+Console.WriteLine(com.Parameters["@compname"].Value);
+
+cn.Close();
